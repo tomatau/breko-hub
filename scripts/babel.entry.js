bb426ef9fb940b8f2512d01d3cc9011e406f2a76
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2015-10-11 16:03:46 +0100

    refactored to include build script

--- scripts/babel.entry.js
+++ scripts/babel.entry.js
@@ -1,2 +1,16 @@
-const argv = require('yargs').argv
-require(`./${(argv.development) ? 'development.es6' : 'serve.es6'}`)
+const argv = require('yargs').argv;
+
+const file = (()=>{
+  switch(true) {
+  case argv.development:
+    return 'development.es6';
+  case argv.build:
+    return 'build.es6';
+  default:
+  case argv.production:
+  case argv.serve:
+    return 'serve.es6';
+  }
+})()
+
+require(`./${file}`)
