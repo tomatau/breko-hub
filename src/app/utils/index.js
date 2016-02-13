205a7d00640cd6041e39b26bea86f0115b8d5615
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2016-02-13 20:41:34 +0000

    moved types-to-reducer to npm package

--- src/app/utils/index.js
+++ src/app/utils/index.js
@@ -1,4 +1,4 @@
 export get from './get'
-export typeToReducer from './typeToReducer'
+export typeToReducer from 'type-to-reducer'
 export { fetch } from './fetch'
 export { isNodeInTree, isPromise, isBrowser } from './predicates'
