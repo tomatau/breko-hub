7cd81205b8fdc7c479a20a2dddb291fd536d886f
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2016-02-28 21:56:05 +0000

    flash saga also handles remove message

--- src/app/sagas/index.js
+++ src/app/sagas/index.js
@@ -1,7 +1,7 @@
 import { delay } from 'lodash'
-import { call, put, fork, take } from 'redux-saga/effects'
+import { call, put, fork, take, race } from 'redux-saga/effects'
 import * as selectors from 'app/selectors'
-import { removeMessage } from 'app/actions/flash'
+import { removeMessage, ADD_MESSAGE, REMOVE_MESSAGE } from 'app/actions/flash'
 
 const DAEMON = true
 const log = {
@@ -12,20 +12,28 @@ const wait = (time) => new Promise(resolve => delay(resolve, time))
 
 function * timeoutRemoveFlash(nextFlash) {
   if (nextFlash) {
-    yield call(wait, 4000)
-    yield put(removeMessage(nextFlash.id))
+    const { removed } = yield race({
+      timeout: call(wait, 4000),
+      removed: take(action =>
+        action.type === REMOVE_MESSAGE
+        && action.id === nextFlash.id
+      ),
+    })
+    if (!removed) {
+      yield put(removeMessage(nextFlash.id))
+    }
   }
 }
 
 function * takeFlashMessages() {
   while (DAEMON) {
-    const action = yield take('flash/ADD_MESSAGE')
+    const action = yield take(ADD_MESSAGE)
     log.sagas('Flash added, saga will remove it automatically')
     yield fork(timeoutRemoveFlash, action.payload)
   }
 }
 
-export default function* rootSaga(getState) {
+export default function * rootSaga(getState) {
   yield fork(timeoutRemoveFlash, selectors.nextFlashMessage(getState()))
   yield fork(takeFlashMessages)
 }
