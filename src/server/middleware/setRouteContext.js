137afea667c549d414190dc8769e45ec99b263f0
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2016-02-28 21:58:51 +0000

    pickup flash messages created from client in server render

--- src/server/middleware/setRouteContext.js
+++ src/server/middleware/setRouteContext.js
@@ -1,6 +1,8 @@
 import { RouterContext, match } from 'react-router'
 import { trigger } from 'redial'
 import { history } from 'app/services/history'
+import * as selectors from 'app/selectors'
+import { store as clientStore } from 'app/services/store'
 
 export default function(makeRoutes) {
   return function *(next) {
@@ -12,11 +14,14 @@ export default function(makeRoutes) {
           location: history.createLocation(this.request.url),
         }, (error, redirect, renderProps) => {
           if (redirect) {
-            // todo: figure out how to persist client redirect state on server render
-            this.addFlash('You may not view the private route!!', 'error')
-            return reject(this.redirect(redirect.pathname + redirect.search))
-          } else if (error)
+            transferFlashMessages.call(this)
+            return reject(this.redirect(`${redirect.pathname}${redirect.search}`))
+          } else if (!renderProps) {
+            this.status = 404
+            return resolve(<p>Not found</p>)
+          } else if (error) {
             return reject(this.throw(error.message))
+          }
 
           trigger('prefetch', renderProps.components, {
             dispatch: store.dispatch,
@@ -31,6 +36,13 @@ export default function(makeRoutes) {
     } catch (error) {
       if (error == null) return // redirecting
       throw error
+    }
+
+    function transferFlashMessages() {
+      const nextFlashMessage = selectors.nextFlashMessage(clientStore.getState())
+      if (nextFlashMessage) {
+        this.addFlash(nextFlashMessage.message, nextFlashMessage.type)
+      }
     }
   }
 }
