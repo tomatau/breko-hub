d8cf040da2d67ac9cb0bd58ee014b724c486411d
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2016-09-04 14:53:27 +0100

    better text in comment

--- src/app/components/HeadNavigation.js
+++ src/app/components/HeadNavigation.js
@@ -1,7 +1,7 @@
 import { IndexLink, Link } from 'react-router'
 import styles from './HeadNavigation.module.scss'
 
-// Putting this inside a container will break activeClassName
+// Putting this inside a connect will break activeClassName
 // unless you also subscribe to changes to routing state or context
 export default class HeadNavigation extends React.Component {
   render() {
