7dd6072b28c7ba8a1e05d1c92f40f6576c3b44cb
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2017-09-03 18:28:19 +0100

    updated version of nhf

--- test/node-setup.js
+++ test/node-setup.js
@@ -1,6 +1,5 @@
 // This file ensures JSDOM is loaded before React is included
-
-const path = require('path')
+const nhf = require('node-hook-filename')
 
 delete process.env.DEBUG
 process.env.NODE_ENV = 'test'
@@ -14,9 +13,5 @@ require('babel-core/register')({
 
 require('../src/helpers/cssModulesHook')
 require('../src/helpers/globalJSDOM')
-require('node-hook-filename')(
-  [ '.svg', '.jpg', '.jpeg' ],
-  (request, parent) =>
-    // normalise paths
-    path.resolve(parent.id, '..', request).replace(/.*(?=\/breko-hub)/, '')
-)
+
+nhf([ /\.svg/, /\.jpg/, /\.jpeg/ ], nhf.normalize)
