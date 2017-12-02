34452f449a72338d77e418627d6e167afc64dcc9
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2017-12-02 19:21:40 +0000

    ensure loadable-modules.json is stubbed in tests

--- test/node-setup.js
+++ test/node-setup.js
@@ -18,4 +18,12 @@ require('babel-core/register')({
 require('../src/helpers/css-modules-hook')
 require('../src/helpers/global-jsdom')
 
-nhf([ /\.svg/, /\.jpg/, /\.jpeg/ ], nhf.normalize)
+const loadableRegex = /loadable-modules\.json/
+
+nhf(
+  [ /\.svg/, /\.jpg/, /\.jpeg/, loadableRegex ],
+  (request, parent) =>
+    loadableRegex.test(request)
+      ? {}
+      : nhf.normalize(request, parent)
+)
