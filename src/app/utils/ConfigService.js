6e022eeecce8921f57a5d76e59b391a1f8cf76a3
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2018-09-02 17:35:42 +0100

    adds util.ConfigService

---
+++ src/app/utils/ConfigService.js
@@ -0,0 +1,43 @@
+const ensureObject = val => (typeof val === 'string' ? JSON.parse(val) : val)
+
+export class _ConfigService {
+  _env = '';
+  _vars = {};
+
+  setEnv(env) {
+    this._env = env
+  }
+
+  getEnv() {
+    return this._env
+  }
+
+  isEnv(...envs) {
+    return envs.includes(this._env)
+  }
+
+  setVars(vars) {
+    this._vars = ensureObject(vars)
+  }
+
+  getVars() {
+    return this._vars
+  }
+
+  assignVars(vars) {
+    this._vars = {
+      ...this._vars,
+      ...ensureObject(vars),
+    }
+  }
+
+  set(key, val) {
+    this._vars[key] = val
+  }
+
+  get(key) {
+    return this._vars[key]
+  }
+}
+
+export default new _ConfigService()
