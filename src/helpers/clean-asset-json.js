c3992113221430bf231d1ea806d00045d818a6ee
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2018-09-30 16:09:32 +0100

    moved many dependencies into devDeps, reshuffled dynamic imports to use require()

--- src/helpers/clean-asset-json.js
+++ src/helpers/clean-asset-json.js
@@ -1,11 +1,10 @@
 import { ASSET_FILE } from 'config/paths'
 import { isEnv } from 'app/utils/predicates'
-import rimraf from 'rimraf'
 
 const log = debug('clean-assets')
 
 if (isEnv('development')) {
-  rimraf(ASSET_FILE, err => {
+  require('rimraf').default(ASSET_FILE, err => {
     if (err) {
       log(err)
       process.exit(1)
