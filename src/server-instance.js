c41c94c00937cf431f8a7df0a7575cc3bf2aba44
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2019-09-30 20:27:11 +0100

    moves robots.txt into assets directory and removes webpack plugin

--- src/server-instance.js
+++ src/server-instance.js
@@ -4,6 +4,7 @@ import convert from 'koa-convert'
 import session from 'koa-session-store'
 import logger from 'koa-logger'
 import favicon from 'koa-favicon'
+import serve from 'koa-static'
 import { ASSETS } from 'config/paths'
 import sessionFlashArray from 'server/middleware/session-flash-array'
 import handleError from 'server/middleware/handle-error'
@@ -14,6 +15,7 @@ app.keys = [ 'd0n7', '7311', '4ny0n3' ]
 
 app.use(compress())
 app.use(favicon(`${ASSETS}/favicon.ico`))
+app.use(serve(ASSETS))
 app.use(convert(session()))
 app.use(sessionFlashArray())
 
